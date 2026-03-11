581618fe5a1eafde0a7bb7b8fb127220c39f828c
João Vítor Fernandes Dias <joaovitorfd2000@gmail.com>
2026-03-11 16:38:25 -0300

    Cloud init script

--- Files/current/scripts/cloud_init.sh
+++ Files/current/scripts/cloud_init.sh
@@ -6,6 +6,10 @@ packages:
   - curl
 
 runcmd:
+  - apt update
+  - apt install -y git docker.io curl
   - git clone https://github.com/jvfd3/FastFreeMinecraftServer.git /opt/FastFreeMinecraftServer
   - chmod +x /opt/FastFreeMinecraftServer/inside_init
-  - CF_API_KEY="$$2a$10$$<API_KEY_HERE>" /opt/FastFreeMinecraftServer/inside_init
+  - mkdir -p /root/.docker/cli-plugins
+  - curl -SL https://github.com/docker/compose/releases/download/v2.27.0/docker-compose-linux-aarch64 -o /root/.docker/cli-plugins/docker-compose
+  - chmod +x /root/.docker/cli-plugins/docker-compose
